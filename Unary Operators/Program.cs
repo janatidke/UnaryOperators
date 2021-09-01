b9aa2392de71182d21c68d02463b9f2dd0180fb9
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 18:12:33 +0530

    Add project files.

---
+++ Unary Operators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UnaryOperators
+{
+    class UnaryOperator
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 3;
+
+            b = a++;
+            Console.WriteLine("The value of b is" + b);
+            Console.WriteLine("The  Post-increment value of a" + a);
+
+
+            b = ++a;
+            Console.WriteLine("The value of b is" + b);
+            Console.WriteLine("The Pre-increment value of a " + a);
+
+
+            b = a--;
+            Console.WriteLine("The value of b is" + b);
+            Console.WriteLine("The  Post-Decrement value of a " + a);
+
+
+            b = --a;
+            Console.WriteLine("The value of b is" + b);
+            Console.WriteLine("The Pre-Decrement value of a " + a);
+
+        }
+    }
+}
+
